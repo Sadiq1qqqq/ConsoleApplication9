cd118e093dc5743d53fac866562f91b51243dd84
Sadiq Quliyev <sadiq000k@gmail.com>
2024-07-27 23:37:23 +0400

    Proje dosyası ekle.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,79 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+	/*do
+	{
+		cout << "Edded daxil edin: ";
+		double num;
+		cin >> num;
+
+		cout << "Eded daxil edin: ";
+		double num2;
+		cin >> num2;
+		cout << "+,-,*,/ daxil edin : ";
+		char op;
+		cin >> op;
+		double total;
+		if (op == '+') {
+			total = num + num2;
+		}
+		else if (op == '-') {
+			total = num - num2;
+		}
+		else if (op == '*') {
+			total = num * num2;
+
+		}
+		else if (op == '/') {
+			total = num / num2;
+		}
+
+		}*/
+
+
+
+
+
+
+
+
+	/*do
+	{
+		double m;
+		cout << "Manat elave edin:";
+		cin >> m;
+		unsigned short int cho;
+		cout << "rubl(1),dollar(2), euro(3) ve funt(4): ";
+		cin >> cho;
+		if (cho == 1)
+		{
+			double r;
+			r = m * 51.9;
+			cout << "rubl = " << r << endl;
+		}
+		else if (cho == 2)
+		{
+			double d;
+			d = m / 1.7;
+
+			double e;
+			e = m / 1.84;
+			cout << "euro = " << e << endl;
+		}
+		else if (cho == 4)
+		{
+			double f;
+			f = m / 2.19;
+			cout << "funt = " << f << endl;
+		}
+		else {
+			cout << "proqram dayandi." << endl;
+			break;
+		}
+	}
+
+}*/
